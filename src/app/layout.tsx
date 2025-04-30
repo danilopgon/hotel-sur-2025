f0d5fd840bc28ebb4ba395e345bcae10110deb49
danilopgon <dani.lopezgonzalez.94@gmail.com>
2025-04-30 17:46:31 +0200

    Agregar favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,16 @@ import './globals.css';
 export const metadata: Metadata = {
   title: 'Hotel Sur',
   description: 'Banda de rock y electrónica de Tarancón (Cuenca)',
+  icons: {
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+  },
+  manifest: '/site.webmanifest',
 };
 
 export default function RootLayout({
